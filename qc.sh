33ae47d9af5a511b336f3af4f783394ec0ef3e9f
Cyril Robin <cyril.robin@laas.fr>
2013-08-15 00:37:59 +0200

    Quick Calculator qc (Python)

---
+++ qc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# code for Linux 
+# taken from http://danielcorin.github.io/blog/2013/07/23/qc/
+# Use to easily compute some math operation
+
+# Standard version with math import for advanced mathematical features
+python -c "from math import *; print $1"
