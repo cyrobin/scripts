bf21d9becda67a560f52478c0192b456b51a5243
Cyril Robin <cyril.robin@laas.fr>
2014-12-16 11:25:31 +0100

    [Converter] Update to avconv (ffmpeg is deprecated)

--- convert_flv2mp3.sh
+++ convert_flv2mp3.sh
@@ -6,13 +6,15 @@
 echo 'Converting .flv files to .mp3 (192kbps)'
 for filename in *.flv #Traverse all files in directory.
 do
-ffmpeg -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.flv}".mp3
+# ffmpeg -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.flv}".mp3
+avconv -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.flv}".mp3
 done
 
 echo 'Converting .mp4 files to .mp3 (192kbps)'
 for filename in *.mp4 #Traverse all files in directory.
 do
-ffmpeg -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.mp4}".mp3
+# ffmpeg -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.mp4}".mp3
+avconv -i "$filename" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "${filename%%.mp4}".mp3
 done
 
 exit 0
